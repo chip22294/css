8c143765d6e8b3aa989a629167ede052cefb2eac
chip22294 <chip22294@gmail.com>
2023-06-25 18:29:54 +0300

    Lesson 8 complete

--- src/route/index.js
+++ src/route/index.js
@@ -16,6 +16,16 @@ router.get('/', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+router.get('/selector', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('selector', {
+    layout: 'selector',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
